31f277fcea5e4f8686a3d472feebbc30c404b810
Divyansh Kumar <jnvdiv2109@gmail.com>
2026-09-23 19:17:10 +0530

    Update: 1658 - Minimum Operations to Reduce X to Zero (Medium)

--- Medium/minimum-operations-to-reduce-x-to-zero/solution.cpp
+++ Medium/minimum-operations-to-reduce-x-to-zero/solution.cpp
@@ -3,10 +3,11 @@ public:
     int minOperations(vector<int>& nums, int x) {
         int count = 0;
         while(x>0){
+            if(nums.size()>0){
             int a = nums.front();
             int b = nums.back();
             if(a>=b){
-                if(x>a){
+                if(x>=a){
                     x = x-a;
                     count++;
                     nums.erase(nums.begin());
@@ -18,7 +19,7 @@ public:
                 }
             }
             else{
-                if(x>b){
+                if(x>=b){
                     x = x-b;
                     count++;
                     nums.pop_back();
@@ -28,6 +29,7 @@ public:
                     count++;
                     nums.erase(nums.begin());
                 }
+            }
             }
         }
         if(x==0) return count;

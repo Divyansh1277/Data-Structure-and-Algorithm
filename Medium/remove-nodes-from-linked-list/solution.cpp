d94c09d46b86c39f19a8f7472bd9fcd5a5169d03
Divyansh Kumar <jnvdiv2109@gmail.com>
2026-09-23 20:28:44 +0530

    Add: 2487 - Remove Nodes From Linked List (Medium)

---
+++ Medium/remove-nodes-from-linked-list/solution.cpp
@@ -0,0 +1,46 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* removeNodes(ListNode* head) {
+        ListNode* temp = head;
+        ListNode* temp1 = head;
+        ListNode* prev = head;
+        while(prev->next!=nullptr){
+            while(temp1!=NULL){
+                if(head->val>=temp1->val){
+                    temp1 = temp1->next;
+                }
+                else{
+                    head = temp1;
+                }
+            }
+            prev = head;
+            temp = head->next;
+            temp1 = head->next;
+            while(temp!=nullptr){
+                if(temp->val>=temp1->val){
+                    temp1 = temp1->next;
+                }
+                else{
+                    prev->next = temp1;
+                    temp = temp1;
+                }
+                if(temp1==nullptr && temp1!=nullptr){
+                    prev = prev->next;
+                    temp = temp->next;
+                    temp1 = temp;
+                }
+            }
+        }
+
+    }
+};

0d38ffda419bc8d56724d53b4e46364e117fff83
Divyansh Kumar <jnvdiv2109@gmail.com>
2026-09-24 19:03:38 +0530

    Update: 3550 - Smallest Index With Digit Sum Equal to Index (Easy)

--- Easy/smallest-index-with-digit-sum-equal-to-index/solution.cpp
+++ Easy/smallest-index-with-digit-sum-equal-to-index/solution.cpp
@@ -2,16 +2,20 @@ class Solution {
 public:
     int smallestIndex(vector<int>& nums) {
         int count = 0;
+        int sum;
         for(int i=0;i<nums.size();i++){
-            int sum = 0;
+            sum = 0;
             while(nums[i]>0){
                 int a = nums[i]%10;
                 sum += a;
                 nums[i] /= 10;
             }
             if(sum==i){
-                return sum;
+                count++;
+                break;
             }
         }
+        if(count==0) return -1;
+        else return sum;
     }
 };

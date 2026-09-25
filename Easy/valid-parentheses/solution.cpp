4954af22d10bfbc81a47f085cd3220d8df093b60
Divyansh Kumar <jnvdiv2109@gmail.com>
2026-09-25 10:18:01 +0530

    Update: 20 - Valid Parentheses (Easy)

--- Easy/valid-parentheses/solution.cpp
+++ Easy/valid-parentheses/solution.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     bool isValid(string s) {
         int count = 0;
-        for(int i=0;i<s.size()-1;i++){
+        for(int i=0;i<s.size()-1;i = i+2){
             if(s[i]=='(' && s[i+1]==')') continue;
             else if(s[i]=='[' && s[i+1]==']') continue;
             else if(s[i]=='{' && s[i+1]=='}') continue;

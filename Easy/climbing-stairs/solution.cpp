d251878e6b86e2754ca2b17ca6a2bb5d54077a20
Divyansh Kumar <jnvdiv2109@gmail.com>
2026-09-25 18:27:16 +0530

    Update: 70 - Climbing Stairs (Easy)

--- Easy/climbing-stairs/solution.cpp
+++ Easy/climbing-stairs/solution.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int climbStairs(int n) {
         if(n==1 || n==2) return n;
-        int i=1,j=2;k=0;
+        int i=1,j=2,k=0;
         for(int l=3;l<=k;l++){
             k = i+j;
             i = j;
